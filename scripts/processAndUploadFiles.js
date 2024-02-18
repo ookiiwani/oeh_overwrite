f730f4a02cdcef83922c8a3501a659a6f20a810c
Max <max@localhost.localdomain>
2024-02-18 18:56:40 +0100

    nice

--- scripts/processAndUploadFiles.js
+++ scripts/processAndUploadFiles.js
@@ -1,23 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
+const fetch = require('node-fetch');
+require('dotenv').config();
 
-const directoryPath = path.join( __dirname, '../overwrite' );
+const directoryPath = path.join(__dirname, '../overwrite');
 const result = {};
 
 fs.readdirSync(directoryPath).forEach(file => {
     const fullPath = path.join(directoryPath, file);
     const stats = fs.statSync(fullPath);
 
-    // Überprüfen, ob der Pfad zu einer Datei führt
     if (stats.isFile()) {
         const content = fs.readFileSync(fullPath, 'utf8');
 
-        // Dateinamen in einen Pfad umwandeln und Dateiendung entfernen
-	const parsed = path.parse(file);
-        console.log( "filename:", parsed.name, ", extension:", parsed.ext );
+        const parsed = path.parse(file);
+        console.log("filename:", parsed.name, ", extension:", parsed.ext);
 
-        // Den Inhalt basierend auf der Dateiendung verarbeiten
         let value;
         if (parsed.ext === '.json') {
             value = JSON.parse(content);
@@ -25,27 +24,29 @@ fs.readdirSync(directoryPath).forEach(file => {
             value = content;
         }
 
-        // Pfad im Ergebnisobjekt erstellen und Wert setzen
         _.set(result, parsed.name, value);
     } else {
         console.log(`${fullPath} ist ein Verzeichnis und wird übersprungen.`);
     }
 });
 
-const data = JSON.stringify(result);
+// Hier wird angenommen, dass `oeh.json` der Name der Datei ist, die Sie überschreiben möchten.
+const dataToSend = JSON.stringify({
+    filename: 'oeh.json',
+    content: JSON.stringify(result)
+});
+
+const token = process.env.AUTHORIZATION_TOKEN;
 
 fetch('https://api.schickl.app/oeh.schickl.app/overwrite?filename=oeh.json', {
-    method: 'POST',
-    body: data,
-    headers: { 'Content-Type': 'application/json' }
+    method: 'PUT',
+    body: dataToSend,
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+    }
 })
 .then(response => response.json())
 .then(json => console.log(json))
-.catch(err => console.error('Fehler beim Senden der Daten:', err));
-
-// Das resultierende Objekt als JSON in eine Datei schreiben
-const outputPath = path.join(__dirname, 'result.json');
-fs.writeFileSync(outputPath, JSON.stringify(result, null, 2), 'utf8');
-
-console.log('Die Dateien wurden erfolgreich in eine JSON-Datei zusammengeführt.');
+.catch(err => console.error('Error:', err));
 
